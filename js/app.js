fbe706c7d0b9eac7d1d53e32eef7df6e54f0fc06
Tizzy <tizzymac@icloud.com>
2020-08-12 12:28:41 -0600

    Fixes bug

--- js/app.js
+++ js/app.js
@@ -1,15 +1,16 @@
 
 var i = 0;
-var dwight = "DWIGHT "
-
+var dwight = "DWIGHT ";
+var fullText = "untitled folder";
 
 var inputBox = document.getElementById('doc');
+inputBox.innerHTML = fullText;
 inputBox.onkeyup = function(){
-	var currentText = document.getElementById('doc').innerHTML;
-    // document.getElementById('doc').innerHTML = (currentText + nextLetter()).substr(1);
-    document.getElementById('doc').innerHTML = (currentText + dwight.charAt(i)).substr(1);
+    fullText = fullText + dwight.charAt(i);
+    inputBox.innerHTML = fullText
     i = (i + 1) % dwight.length;
 }
+
 inputBox.contentEditable = true;
 inputBox.focus();
 
